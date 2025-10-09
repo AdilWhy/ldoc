41e1f41390705140bdf3b7d282cee1a2ed4a389e
Adil Yerzhanuly <adil270120@gmail.com>
2025-10-09 18:37:15 +0500

    feat: SIS3

---
+++ sis3/smoke_test.sh
@@ -0,0 +1,13 @@
+systemctl status nginx
+
+# Check Hugo binary
+hugo version
+
+# Check Podman
+podman ps
+
+# Check Prometheus
+prometheus --version
+
+# Check Node Exporter
+curl http://localhost:9100/metrics | head -n 5
